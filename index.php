398aa79212a8dc25a5c61cbdd28a8d54e2417f1c
majk <yuryshynets.m@gmail.com>
2016-11-28 11:13:16 +0200

    commit one

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MikeYuri Portfolio</title>
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+    <div class="site-wrapper">
+        <div class="home-wraper">
+       		<div id="header">
+        		<div class="container">
+        			<div class="row">
+        			    <div class="col-md-2 col-md-offset-10">
+                            <div class="lang-box">
+                                <ul class="language">
+                                    <li id="ru">ru</li>
+                                    <li id="ua">ua</li>
+                                    <li id="eng">eng</li>
+                                </ul>
+                            </div>
+        			    </div>
+        				<div class="col-md-12">
+    	    				<div class="logo-wraper">
+    	    					<div class="logo">
+    	    						<p>Mike <span>Yuryshynets</span></p>
+    	    					</div>
+    	    					<div class="nav-wraper">
+    	    						<ul class="nav">
+    	    							<li class="activ">Главная</li>
+    	    							<li>Услуги</li>
+    	    							<li>Навыки</li>
+    	    							<li>Портфолио</li>
+    	    							<li>Отзывы</li>
+    	    							<li>Контакты</li>
+    	    						</ul>
+    	    					</div>
+                                <div class="clear"></div>
+    	    				</div>
+        				</div>
+                    </div>
+        		</div><!--.container-->
+        	</div><!--#header-->
+            <div class="home">
+                <div class="container">
+                    <div class="row vcenter">
+                        <div class="col-md-12 text-center my-present">
+                            <p>Здравствуйте, меня зовут Михаил Юришинец, я</p>
+                            <h1>FRONT-END DEWELOPER</h1>
+                        </div>
+                    </div> <!--.row-->
+                    <div class="bottom-down-box">
+                        <div class="bottom-down">
+                            <span class="arrow-bot"></span>
+                        </div>
+                    </div>
+                </div><!--.container-->
+            </div><!--.home--> 
+        </div><!--.home-wraper-->
+    </div><!--.site-wrapper-->
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
