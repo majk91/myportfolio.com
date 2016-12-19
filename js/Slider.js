3ffc9d741468022a0fb6df0a67d71c2dc1eb630c
majk <yuryshynets.m@gmail.com>
2016-12-19 17:57:47 +0200

    slider

--- js/Slider.js
+++ js/Slider.js
@@ -3,10 +3,78 @@ function Slider(settings) {
 	var sliderContent = $('.slider-content').eq(0);
 	var sliderItems = $('.slider-item');
 	var sliderWidth = sliderContent.width();
-	var singleSlideWidth = sliderWidth/settings.slidersToShow;
+	var singleSlideWidth = 0;
+	var sliderButtons = $('.slider-button');
+	var self = this;
 
-	for(var i = 0; i < sliderItems.length; i++){
-		sliderItems[i].style.width= singleSlideWidth + 'px';
-		sliderItems[i].style.left =(singleSlideWidth*i)+ 'px';
+	var showSlides = function(a){
+		for(var i = 0; i < sliderItems.length; i++){
+			if(sliderItems.eq(i).css('left') !== "auto"){
+				var current = parseFloat(sliderItems.eq(i).css("left"));
+				sliderItems.eq(i).css("left", (current + (singleSlideWidth * a)) + 'px');
+			}else{
+				sliderItems.eq(i).css("width", singleSlideWidth + 'px');
+				sliderItems.eq(i).css("left", (singleSlideWidth * i) + 'px');
+			}
+		}
+
 	}
+
+	this.move_left = function(){	
+		showSlides(-1);
+	}
+	this.move_right = function(){
+		showSlides(1);
+	}
+
+
+//ПРОВЕРКА НА ПОСЛЕДНИЙ ЭЛЕМЕНТ КОЛЕКЦИИ
+//если после элемент последний то кнопка блокируется и добавляеться оформление
+	function inspectLast(){
+		for(var i = 0; i < sliderButtons.length; i++){
+			sliderButtons.eq(i).attr('disabled', false);
+		}
+		if($(event.target).attr('data-action') == "left"){
+			if(parseFloat(sliderItems.eq(sliderItems.length - 1).css('left')) < sliderWidth){
+				$(event.target).attr('disabled', true);
+			}
+		}else{
+			if(parseFloat(sliderItems.eq(0).css('left')) >= 0 - singleSlideWidth){
+				$(event.target).attr('disabled', true);
+			}
+		}
+	}
+
+//Функция запускает прокрутку в слайдере
+	function reiteration() {
+		for(var i = 0; i < sliderItems.length; i++){
+			if(parseFloat(sliderItems.eq(0).css('left')) > 0){
+				sliderItems.eq(sliderItems.length - (i + 1)).css('left', singleSlideWidth * i);
+			}
+			
+		}
+	}
+
+
+	var init = function (){
+		if(!settings.slidesToShow || isNaN(+settings.slidesToShow)){
+			settings.slidesToShow = 1;
+		}
+		singleSlideWidth = sliderWidth/settings.slidesToShow;
+
+		showSlides(1);
+
+		for(var i = 0; i < sliderButtons.length; i++){
+			sliderButtons.eq(i).on('click', function(){
+				self['move_' + $(this).attr('data-action')]();
+				if(settings.infinite == 0){
+					inspectLast();
+				}else{
+					reiteration();
+				}
+			});
+		}
+	}
+
+	init();
 }
--- parts/apps/mySlider.php
+++ parts/apps/mySlider.php
@@ -1,15 +1,25 @@
 <div class="slider" id="slider1">
 	<div class="slider-content">
-		<div class="slider-item"></div>
-		<img src="image/img/11111.jpg" alt="big boss">
-		<div class="slider-item"></div>
-		<img src="image/img/2222.jpg" alt="big boss">
-		<div class="slider-item"></div>
-		<img src="image/img/3333.jpg" alt="big boss">
-		<div class="slider-item"></div>
-		<img src="image/img/11111.jpg" alt="big boss">
-		<div class="slider-item"></div>
-		<img src="image/img/2222.jpg" alt="big boss">
+		<div class="slider-item">
+			<div class="23">1</div>
+			<img src="image/img/11111.jpg" alt="big boss">
+		</div>
+		<div class="slider-item">
+			<div class="23">2</div>
+			<img src="image/img/2222.jpg" alt="big boss">
+		</div>
+		<div class="slider-item">
+			<div class="23">3</div>
+			<img src="image/img/3333.jpg" alt="big boss">
+		</div>
+		<div class="slider-item">
+			<div class="23">4</div>
+			<img src="image/img/11111.jpg" alt="big boss">
+		</div>
+		<div class="slider-item">
+			<div class="23">5</div>
+			<img src="image/favicon1.png" alt="big boss">
+		</div>
 	</div>
 	<button class="slider-button slider-button-prev" data-action="left">prev</button>
 	<button class="slider-button slider-button-next" data-action="right">next</button>
@@ -17,7 +27,7 @@
 <script type="text/javascript">
 	var s1 = new Slider({
 		sliderId: 'slider1',
-		sliderToShow:4,
-		infinite: false
+		slidesToShow: 3,
+		infinite: 1
 	});
 </script>
