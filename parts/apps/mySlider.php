96a42c7f783fd3bffc75415062e7a6d2c1a860e9
majk <yuryshynets.m@gmail.com>
2016-12-22 18:08:17 +0200

    slider v2 not ready

--- parts/apps/mySlider.php
+++ parts/apps/mySlider.php
@@ -16,16 +16,16 @@
 			<img src="image/favicon1.png" alt="big boss">
 		</div>
 	</div>
-	<button class="slider-button slider-button-prev" data-action="left">prev</button>
-	<button class="slider-button slider-button-next" data-action="right">next</button>
+	<button class="slider-button slider-button-prev" data-action="left"></button>
+	<button class="slider-button slider-button-next" data-action="right"></button>
 </div>
-<!--slidesToShow: 3, - сколько картинок выводить
-	infinite: 1 - цыкличный слайдер
-			  0 - не цыкличный слайдер -->
+<!--    slidesToShow: 3, - сколько картинок выводить
+	    infinite: 1 - цыкличный слайдер
+	    		  0 - не цыкличный слайдер -->
 <script type="text/javascript">
 	var s1 = new Slider({
 		sliderId: 'slider1',
 		slidesToShow: 3,
-		infinite: 1
+		infinite: 0
 	});
 </script>
