2490d435578e0a91c9604b90d74d526893f7d7bf
mike <yuryshynets.m@gmail.com>
2016-12-25 22:20:13 +0200

    mobile v1

--- parts/_1header.php
+++ parts/_1header.php
@@ -36,12 +36,12 @@
         				<div class="col-xs-12 col-sm-12 col-md-12">
     	    				<div class="logo-wraper">
                                 <div class="row">
-                                    <div class="col-xs-10 col-sm-12 col-md-3">
+                                    <div class="col-xs-8 col-sm-12 col-md-3">
             	    					<div class="logo">
             	    						<p> <a href="">Mike <span>Yuryshynets</span></a></p>
             	    					</div>
                                     </div>
-                                    <div id="nav-wraper" class="col-xs-2 col-sm-12 col-md-9">
+                                    <div id="nav-wraper" class="col-xs-4 col-sm-12 col-md-9">
                                         <ul class="nav hidden-xs">
                                             <li><a href="#" class="activ">Главная</a></li>
                                             <li><a href="#">Услуги</a></li>
@@ -52,7 +52,20 @@
                                             <li><a href="#">Контакты</a></li>
                                         </ul>
                                         <div class="visible-xs">
-                                            <span id="menu-btn" class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+                                            <div class="dropdown">
+                                                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                    <span id="menu-btn" class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+                                                </button>
+                                                <ul class="dropdown-menu dropdown-menu-righ dropdownMenu1-item" aria-labelledby="dropdownMenu1">
+                                                    <li><a href="#" class="activ">Главная</a></li>
+                                                    <li><a href="#">Услуги</a></li>
+                                                    <li><a href="#">Клиенты</a></li>
+                                                    <li><a href="#">Резюме</a></li>
+                                                    <li><a href="#">Портфолио</a></li>
+                                                    <li><a href="#">Отзывы</a></li>
+                                                    <li><a href="#">Контакты</a></li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
