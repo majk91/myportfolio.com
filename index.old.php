93eb00d78eb74fc035702afe87f45d93b44f715e
mike <yuryshynets.m@gmail.com>
2017-01-22 22:01:34 +0200

    save

--- index.old.php
+++ index.old.php
@@ -19,28 +19,29 @@
 
 </head>
 <body>
+
     <div class="site-wrapper">
         <div class="home-wraper">
         
-<?php /*require_once("parts/_1header.php") */?>
+<?php require_once("parts/_1header.php") ?>
 
-<?php /*require_once("parts/_2home.php") */?>
+<?php require_once("parts/_2home.php") ?>
 
         </div><!--.home-wraper-->
 
-<?php /*require_once("parts/_3servis.php") */?>
+<?php require_once("parts/_3servis.php") ?>
 
-<?php /*require_once("parts/_4clients.php") */?>
+<?php require_once("parts/_4clients.php") ?>
 
 <?php /*require_once("parts/_5rezume.php")*/ ?>
 
-<?php /*require_once("parts/_6myWorks.php") */?>
+<?php require_once("parts/_6myWorks.php") ?>
 
-<?php /*require_once("parts/_7reviews.php") */?>
+<?php require_once("parts/_7reviews.php") ?>
 
-<?php /*require_once("parts/_8contact.php") */?>
+<?php require_once("parts/_8contact.php") ?>
 
-<?php /*require_once("parts/_9footer.php") */?>
+<?php require_once("parts/_9footer.php") ?>
 
 
 
